0bbfae92d1307695b0c1f153290d660c702e998a
Sebastián Gallegos F. <59512190+SebaGallegos@users.noreply.github.com>
2025-09-08 17:12:43 -0300

    Delete nvim directory

--- nvim/lua/sebastian/init.lua
+++
@@ -1,2 +0,0 @@
-require("sebastian.keymaps")
-require("sebastian.opciones")
--- nvim/lua/sebastian/keymaps.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<F2>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-
